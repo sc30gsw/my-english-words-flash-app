7d83b1b649948a8200e6a5ebc5837457ef98051c
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-04-20 15:25:04 +0900

    🏷️ 定数追加

---
+++ src/constants/index.ts
@@ -0,0 +1,20 @@
+export const SIMPLE_FILE_NAMES = [
+  '基礎トレーニング1.csv',
+  '基礎トレーニング4.csv',
+  '日常会話1.csv',
+  '便利な表現1.csv',
+  '受動態1.csv',
+  '受動態2.csv',
+  'to-不定詞1.csv',
+] as const satisfies readonly string[]
+
+export const SYNTAX_FILE_NAMES = [
+  'よく使う表現1.csv',
+  '基礎トレーニング2.csv',
+  '基礎トレーニング3.csv',
+] as const satisfies readonly string[]
+
+export const CONVERSATION_FILE_NAMES = [
+  '会話形式1.csv',
+  '会話形式2.csv',
+] as const satisfies readonly string[]
